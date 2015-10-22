989bafbf98c46a080aa04f251b22fb3dead53e0a
kyunooh <chm073@gmail.com>
2015-10-22 22:41:08 +0900

    add highlight symbol

--- init.el
+++ init.el
@@ -36,4 +36,11 @@
 (require 'helm)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (helm-mode 1)
+
+(require 'highlight-symbol)
+(global-set-key [(control f3)] 'highlight-symbol)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+
 ;;; init.el ends here
